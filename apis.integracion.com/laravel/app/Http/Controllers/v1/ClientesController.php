3bdab543124d44123c2b11abebabda67c8504858
RaquelMD <i2125673@continental.edu.pe>
2022-08-09 22:38:50 -0500

    subiendo archivos

---
+++ apis.integracion.com/laravel/app/Http/Controllers/v1/ClientesController.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller\v1;
+ 
+class ClientesController extends Controller
+{
+    public function getall()
+    {
+        return "hola mundo";
+    }
+}
